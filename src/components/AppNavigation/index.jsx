4d1e64a15028cfc5f572e98f8094beb1a5c1b805
Alexthescott <atscott@ucsc.edu>
2022-09-18 12:25:39 -0700

    initial icons added

--- src/components/AppNavigation/index.jsx
+++ src/components/AppNavigation/index.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
+import { ReactComponent as SetBpmIcon } from '../../resources/icons/set_bpm.svg'
+import { ReactComponent as FilterPlaylistIcon } from '../../resources/icons/filter_playlist.svg'
+import { ReactComponent as GeneratePlaylist } from '../../resources/icons/generate_playlist.svg'
+
+
 export default () => (
-    <div className="text-white fixed px-2 bottom-0 right-0 left-0 flex align-center text-center justify-around md:left-0 md:top-0 md:right-auto md:w-100 md:pt-40 md:pr-auto md:h-full md:justify-flex md:flex-d md:flex-col">
+    <div className="text-white bg-stone-900 fixed px-2 bottom-0 right-0 left-0 flex align-center text-center justify-evenly md:left-0 md:top-0 md:right-auto md:w-100 md:pt-40 md:pr-auto md:h-full md:justify-flex md:flex-d md:flex-col">
         <Link className="app-icon" to="/app">
         </Link>
-        <NavLink activeClassName="active" className="text-center" to="/set-bpm">
-            <span className="icon" />
-            <span className="navigation__label">Set Bpm</span>
+        <NavLink activeClassName="active" className="" to="/set-bpm">
+            <SetBpmIcon className="fill-white block mx-auto"/>
+            <span className="text-sm">Set Bpm</span>
         </NavLink>
         <NavLink activeClassName="active" className="text-center" to="/filter-playlist">
-            <span className="icon" />
-            <span className="navigation__label">Filter Playlist</span>
+            <FilterPlaylistIcon className="fill-white block mx-auto" />
+            <span className="text-sm">Filter Playlist</span>
         </NavLink>
         <NavLink activeClassName="active" className="text-center" to="/generate-playlist">
-            <span className="icon" />
-            <span className="navigation__label">Generate Playlist</span>
+            <GeneratePlaylist className="fill-white block mx-auto" />
+            <span className="text-sm">Generate Playlist</span>
         </NavLink>
     </div>
 );
