03640d0573989e43819e7909ad2062f4cc7c0881
Alex <atscott@ucsc.edu>
2022-09-25 17:39:56 -0700

    style and add limits to bpm input

--- src/components/BpmInput/index.jsx
+++ src/components/BpmInput/index.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import localStorage from 'local-storage';
 
-export default function BpmInput({ bpm_type }) {
-    const previous_bpm_state = localStorage.get(bpm_type) != null ? localStorage.get(bpm_type) : "80"
+export default function BpmInput({ bpm_type, default_bpm }) {
+    const previous_bpm_state = localStorage.get(bpm_type) != null ? localStorage.get(bpm_type) : default_bpm
 
     const [bpm, setBpm] = useState(previous_bpm_state);
 
@@ -10,17 +10,41 @@ export default function BpmInput({ bpm_type }) {
         localStorage.set(bpm_type, bpm)
     })
 
+    const handleBpmChange = event => {
+        // limit the size of characters to 3, stop 0 from being the first value
+        let value = event.target.value.slice(0, 3)
+        if (value[0] === "0") value = value.slice(1, 3)
+        setBpm(value);
+    }
+
+    const limitBpmRange = event => {
+        // limit min of 60bpm and max of 260bpm
+        // if we're min bpm, limit outselves to one below current max
+        // if we're max bpm, limit outselves to one above current min
+        let max = 260;
+        let min = 60;
+        if (bpm_type === "min_bpm") {
+            max = Number(localStorage.get("max_bpm"))-1
+        } else {
+            min = Number(localStorage.get("min_bpm"))+1
+        }
+        const value = Math.max(min, Math.min(max, Number(event.target.value)))
+        setBpm(value);
+    }
+
     return(
         <forum>
             <input 
-            className="text-black m-2" 
+            className="m-2 p-2 bg-black border-2 border-white rounded-lg text-center text-2xl" 
             type="number" 
             step="1" 
             pattern="\d+" 
             min="60" 
             max="260" 
+            maxLength={3}
             value={bpm}
-            onChange={(e) => setBpm(e.target.value)}></input>
+            onChange={handleBpmChange}
+            onBlur={limitBpmRange}></input>
         </forum>
     );
 }
--- src/components/SetBpm/index.jsx
+++ src/components/SetBpm/index.jsx
@@ -1,14 +1,15 @@
 import BpmInput from '../BpmInput';
 
 export default () => (
-    <div className='object-center'>
+    <div className='object-center text-center'>
         <div>
-            <h1>Min BPM</h1>
-            <BpmInput bpm_type={"min_bpm"}/>
+            <h1 className='text-2xl'>Min BPM</h1>
+            <BpmInput bpm_type={"min_bpm"} default_bpm={"100"}/>
         </div>
+        <div className='p-3'/>
         <div>
-            <h1>Max BPM</h1>
-            <BpmInput bpm_type={"max_bpm"}/>
+            <h1 className='text-2xl'>Max BPM</h1>
+            <BpmInput bpm_type={"max_bpm"} default_bpm={"120"} />
         </div>
     </div>
 );
