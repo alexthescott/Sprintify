76cde8b3d1f06541e6f6af65c635a17e5ef65c6b
Alex <atscott@ucsc.edu>
2022-09-25 22:07:20 -0700

    swap max and min position in setBpm

--- src/components/SetBpm/index.jsx
+++ src/components/SetBpm/index.jsx
@@ -3,13 +3,13 @@ import BpmInput from '../BpmInput';
 export default () => (
     <div className='object-center text-center'>
         <div>
+            <h1 className='text-2xl'>Max BPM</h1>
+            <BpmInput bpm_type={"max_bpm"} default_bpm={"120"} />
+        </div>
+        <div>
             <h1 className='text-2xl'>Min BPM</h1>
             <BpmInput bpm_type={"min_bpm"} default_bpm={"100"}/>
         </div>
         <div className='p-3'/>
-        <div>
-            <h1 className='text-2xl'>Max BPM</h1>
-            <BpmInput bpm_type={"max_bpm"} default_bpm={"120"} />
-        </div>
     </div>
 );
