b617dd9fa0616e7e07e5b57f6e98e4368d4a493b
Ykrej <stellan125@gmail.com>
2022-11-20 00:34:25 -0800

    Added specific callback redirect

--- src/components/FilterPlaylist.tsx
+++ src/components/FilterPlaylist.tsx
@@ -2,8 +2,9 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { getPlaylists } from '../services/spotify/api'
-import { clearToken } from '../services/spotify/auth'
+import { AUTH_URL, clearToken } from '../services/spotify/auth'
 import { Playlist } from '../services/spotify/models'
+import { cacheRedirect } from '../services/redirect'
 
 
 
@@ -15,13 +16,15 @@ function FilterPlaylist() {
     useEffect(() => {
         if (fetching.current) return
         fetching.current = true
+
         getPlaylists()
             .then((result) => {
                 setPlaylists(result)
             })
             .catch(() => {
                 clearToken()
-                navigate("/login")
+                cacheRedirect('/filter-playlist')
+                window.location.href = AUTH_URL
             })
             .finally(() => {
                 fetching.current = false
