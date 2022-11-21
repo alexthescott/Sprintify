fd1a96c77bb30bf81a8e47301cfaf0c6063044e1
Alex <atscott@ucsc.edu>
2022-11-20 17:16:05 -0800

    Add initial filtered playlists display playlists

--- src/components/FilterPlaylist.tsx
+++ src/components/FilterPlaylist.tsx
@@ -6,7 +6,9 @@ import { AUTH_URL, clearToken } from '../services/spotify/auth'
 import { Playlist } from '../services/spotify/models'
 import { cacheRedirect } from '../services/redirect'
 
-
+function playlistHasSongs(playlist: Playlist) {
+    return playlist.images.length >= 1;
+}
 
 function FilterPlaylist() {
     const navigate = useNavigate()
@@ -32,11 +34,18 @@ function FilterPlaylist() {
     }, [navigate])
 
     return (
-        <>
-            <div>
-                {playlists.map((p) => <div key={p.id}>{p.name}</div>)}
+        <div className="md:ml-20 md:mr-[68px] mx-3">
+            <div className="grid gap-5 grid-cols-3 grid-rows-3 ">
+                {playlists.filter(playlistHasSongs).map((p) => {
+                return( 
+                <div className="flex justify-center">
+                    <div className="rounded-lg shadow-lg bg-stone-900 max-w-sm">
+                        <img className="rounded-t-lg" src={p.images[0].url} alt="" />
+                        <div key={p.id}>{p.name}</div>
+                    </div>
+                </div>)})}
             </div>
-        </>
+        </div>
     )
 }
 
--- src/pages/AppView.tsx
+++ src/pages/AppView.tsx
@@ -9,7 +9,7 @@ function AppView() {
         <div className="pt-0">
             <div className="relative min-h-full pb-12">
                 {/*Popups?*/}
-                <div className="App-content fixed left-0 bottom-4 md:bottom-0 md:left-[52px] w-full flex justify-center items-center h-screen bg-black text-white left-21">
+                <div className="overflow-y-auto App-content fixed left-0 bottom-4 md:bottom-0 md:left-[52px] w-full flex justify-center items-center h-screen bg-black text-white left-21">
                     <Routes>
                         <Route path="*" element={<Navigate to="/set-bpm" replace/>} />
                         <Route path="set-bpm" element={<BpmInput onChange={console.log} />}></Route>{/* Pretty broken right now */}
