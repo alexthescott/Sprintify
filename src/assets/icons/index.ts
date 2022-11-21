e4edd9e120de0992239194eb11db82eb175f7027
Ykrej <stellan125@gmail.com>
2022-11-20 16:54:57 -0800

    added logout and no-token redirect to login

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,11 @@
 import { ReactComponent as SetBpmIcon } from './set_bpm.svg'
 import { ReactComponent as FilterPlaylistIcon} from './filter_playlist.svg'
 import { ReactComponent as GeneratePlaylistIcon } from './generate_playlist.svg'
+import { ReactComponent as LogoutIcon } from './logout.svg'
 
 export {
     SetBpmIcon,
     FilterPlaylistIcon,
-    GeneratePlaylistIcon
+    GeneratePlaylistIcon,
+    LogoutIcon
 }
