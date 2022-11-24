f8c8d077083fddfeec698ab0f225bcf690a6e05b
Ykrej <stellan125@gmail.com>
2022-11-23 16:49:31 -0800

    removed react-modal dependency

--- src/components/PlaylistModal.tsx
+++ src/components/PlaylistModal.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
-import Modal from 'react-modal'
 import { CloseIcon } from '../assets/icons'
 import { Playlist, PlaylistItem } from '../services/spotify/models'
 import PlaylistItemRow from './PlaylistItemRow'
 
-
-Modal.setAppElement("#root")
 
 interface Props {
     isOpen: boolean
@@ -17,26 +14,40 @@ interface Props {
     onClose: () => void
 }
 function PlaylistModal({ isOpen, playlist, items, onClose, onYes, onNo }: Props) {
-    return <Modal
-        isOpen={isOpen}
-        style={{content:{margin: 0, background: "black", padding: 0, overflow: "visible"}}}
-        onRequestClose={onClose}
-    >
-        <div>
-            <div className="flex bg-stone-900 p-5">
-                <h1 className="flex-1 text-white text-lg">{playlist.name}</h1>
-                <CloseIcon className="fill-white" onClick={onClose}/>
-            </div>
-            {/* <div className="px-5">  // Idk if we should render playlist
-                {items.map((item) => <PlaylistItemRow item={item} />)}
-            </div> */}
-            <p className="text-white">Do you want to sort this?</p>
-            <div className="flex">
-                <button className="bg-stone-900 text-white text-lg rounded-sm flex-initial w-32 p-3 m-1" onClick={onYes}>Yes</button>
-                <button className="bg-stone-900 text-white text-lg rounded-sm flex-initial w-32 p-3 m-1" onClick={onNo}>No</button>
+    return (isOpen ? (
+        <>
+        <div className="z-50 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                <div className="bg-black border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none">
+                    <div className="flex items-start justify-between p-5 border-solid rounded-t">
+                        <h3 className="text-white text-l md:text-2xl">{playlist.name}</h3>
+                        <CloseIcon className="fill-white" onClick={onClose} />
+                    </div>
+                    <div className="relative p-6 flex-auto">
+                        <p className="text-sm md:text-xl">Would you like to sort this playlist by bpm?</p>
+                    </div>
+                    <div className="flex items-center justify-end p-6 rounded-b">
+                        <button
+                            className="text-white bg-stone-900 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                            type="button"
+                            onClick={onYes}
+                        >
+                            Yes
+                        </button>
+                        <button
+                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                            type="button"
+                            onClick={onNo}
+                        >
+                            No
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-    </Modal>
+        </>
+    ) : null
+    )
 }
 
 export default PlaylistModal
