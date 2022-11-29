90067a19f029331964570d04613adfbd779e97be
Alex <atscott@ucsc.edu>
2022-11-28 16:42:29 -0800

    additional text formatting

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -10,9 +10,11 @@ function PlaylistCard({ playlist, onClick }: Props) {
     return (
         <div key={playlist.id} className="flex justify-center" onClick={onClick} >
             <div className="rounded-lg shadow-lg bg-stone-900 max-w-sm">
-                <img className="rounded-t-lg aspect-square object-cover" src={playlist.images[0].url} alt="" />
-                <div key={playlist.id}>{playlist.name}</div>
-                <div>{playlist.tracks.total} tracks</div>
+                <div className="rounded-lg aspect-square object-cover m-4 drop-shadow-2xl">
+                    <img className="rounded-lg aspect-square object-cover" src={playlist.images[0].url} alt={playlist.name}/>
+                </div>
+                <div key={playlist.id} className="text-lg font-bold mx-3 -mt-1">{playlist.name}</div>
+                <div className="mx-3 mb-2">{playlist.tracks.total} tracks</div>
             </div>
         </div>
     )
